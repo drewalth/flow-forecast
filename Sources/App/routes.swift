5d46f3e4b0a249d1fbe3751d60de09808c602f4c
drewalth <16584198+drewalth@users.noreply.github.com>
2024-08-26 17:59:40 -0600

    chore: initial poetry setup

--- Sources/App/routes.swift
+++
@@ -1,8 +0,0 @@
-import Vapor
-
-func routes(_ app: Application) throws {
-  app.get { req in
-    req.view.render("index", ["title": "Hello, world!"])
-  }
-  try app.register(collection: ForecastController())
-}
